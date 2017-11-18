719e9ad3778a5f87dd8caf74c8cbab2c7fd3becb
nathanjmcdougall <nathan.j.mcdougall@gmail.com>
2017-11-18 22:29:46 +1300

    Added task rosters for re-occuring tasks

--- task_manager.py
+++ task_manager.py
@@ -17,7 +17,14 @@ days_per_second = 1/(seconds_per_minute*minutes_per_hour*hours_per_day)
 task_log_name = ".task_log"
 
 # The name of the backup file
-backup_log_name = ".task_log_backup"
+backup_task_log_name = ".task_log_backup"
+
+# If this file does not exist, it will not be created.
+# You need to make sure this file exists yourself
+roster_log_name = ".roster_log"
+
+# The name of the backup file
+backup_roster_log_name = ".roster_log_backup"
 
 # The name of the configuration file
 config_name = ".config"
@@ -41,28 +48,32 @@ class Task:
 		self.finished = finished
 
 	@staticmethod
-	def make_task():
-		# Get the name from the user
-		name = input("Please enter in the name for this task: ")
-
-		# We use comma spacing for the log file, so remove any from the name.
-		name = name.replace(",", "")
-		
+	def schedule_task(name, start_time, deadline):		
 		# This is a new task, so clearly we haven't spent time on it
 		hours_spent = 0;
-	
-		# We are starting this task now
-		start_time = dt.now()
-
-		# Get the deadline from the user
-		deadline = get_time("Please enter the deadline for the new task: ")
 
 		# For the moment the finish time is meaningless; use the deadline
 		finish_time = deadline
 
 		# We have not finished a task we have just started
 		finished = False
+
+		# Give back the task
 		return Task(name, hours_spent, start_time, deadline, finish_time, finished)
+
+	@staticmethod
+	def make_task():
+		# Get the name from the user
+		name = get_name("Please enter in the name for this task: ")
+
+		# We are starting this task now
+		start_time = dt.now()
+
+		# Get the deadline from the user
+		deadline = get_time("Please enter the deadline for the new task: ")
+
+		# Give back the task
+		return shedule_task(name, hours_spent, start_time, deadline, finish_time, finished)
 
 	def time_remaining(self):
 		# How much time remains for this task; or negative if we have exceeded the deadline
@@ -80,6 +91,63 @@ class Task:
 		self.hours_spent += 1
 
 ########################################################
+# Roster class
+########################################################
+
+class Roster:
+	def __init__(self, name, next_start_time, duration, period):
+		# Populate the Roster's properties
+		self.name = name
+		self.next_start_time = next_start_time
+		self.duration = duration
+		self.period = period
+
+	@staticmethod
+	def make_roster():
+		# Get the name from the user
+		name = get_name("Please enter in the name for this task roster: ")
+
+		# Get the first start time from the user
+		first_start_time = get_time("Please enter the first start time: ")
+
+		# Get the period
+		period = get_timedelta("Please enter the number of days between each task instance: ")
+
+		# Get the duration from the user
+		duration = get_timedelta("Please enter in the number of days allowed for each task: ")
+
+		# Give back the task
+		return Roster(name, first_start_time, duration, period)
+
+	def check_roster(self):
+		# If the task's start time has elapsed, schedule the task
+		if self.next_start_time <= dt.now():
+			# The deadline occurs a duration worth of time after the start
+			deadline = self.next_start_time + self.duration
+			
+			# Schedule the task
+			new_task = Task.schedule_task(self.name, self.next_start_time, deadline)
+
+			# Prepare the enxt start time
+			self.next_start_time += self.period
+
+			# Report the new task
+			return new_task
+		else:
+			return False
+
+	@staticmethod
+	def check_all_rosters(roster_list, task_list):
+		for roster in roster_list:
+			new_task = roster.check_roster()
+			if new_task:
+				task_list.append(new_task)
+		
+		return task_list
+		
+			
+
+########################################################
 # Input times from the user
 ########################################################
 
@@ -94,7 +162,39 @@ def get_time(request):
 			return dt.strptime(time,"%Y-%m-%d %H:%M")
 		except ValueError:
 			# Report to the user that the string couldn't be parsed
-			print("Sorry, that time you entered could not be interpreted as such. Please use the format [Y-m-d H:M] and try again: ")
+			print("Sorry, that time you entered could not be interpreted as such. Please use the format [YYYY-MM-DD HH:MM] and try again: ")
+
+########################################################
+# Input time intervals from the user
+########################################################
+
+def get_timedelta(request):
+	# Keep trying until we properly parse the input
+	while True:
+		try:
+			# Get the time interval in days from the user
+			time_interval = float(input(request))
+
+			# Check it is positive
+			assert(time_interval >= 0)				
+
+			# Return the interval as a timedelta
+			return td(time_interval)
+		except ValueError:
+			# Report to the user that the string couldn't be parsed
+			print("Sorry, that number of days you entered could not be interpreted as such. Please ensure it is a valid positive number and try again: ")
+
+########################################################
+# Input names from the user
+########################################################
+
+def get_name(request):
+	# Query the user
+	name = input(request)	
+
+	# We use comma spacing for the log file, so remove any from the name.
+	return name.replace(",", "")
+	
 
 ########################################################
 # Blank out a file
@@ -114,10 +214,8 @@ def clear_file(file_name):
 	# Close the file
 	f.close()
 
-	
-
 ########################################################
-# Read in a data file of tasks
+# Read in the data file of tasks
 ########################################################
 
 def read_tasks():
@@ -151,7 +249,7 @@ def read_tasks():
 			finish_time = dt.strptime(finish_time,time_format)
 			finished = (finished == "1")
 		except ValueError:
-			raise ValueError("The opened file could not be parsed.")
+			raise ValueError("The task log could not be parsed.")
 
 		# Create the task for this line
 		task = Task(name, hours_spent, start_time, deadline, finish_time, finished)
@@ -166,12 +264,57 @@ def read_tasks():
 	return task_list
 
 ########################################################
-# Write to the data file
+# Read in the data file of rosters
 ########################################################
 
-def log_tasks(task_list):
+def read_rosters():
+	# Try to read in the roster log file
+	try:
+		roster_file = open(roster_log_name, "r")
+	except IOError:
+		raise IOError("The roster log file could not be opened.")
+
+	# We are going to store rosters in a list
+	roster_list = []
+
+	# Each line in the file corresponds to a task
+	for roster_str in roster_file:
+		# Split each line using spaces into a list
+		roster_str_list = roster_str.split(",")
+
+		# Identify which elements in the list are which
+		name = roster_str_list[0]
+		next_start_time = roster_str_list[1]
+		duration = roster_str_list[2]
+		period = roster_str_list[3]
+
+		# Convert from strings to the correct formats
+		try:
+			next_start_time = dt.strptime(next_start_time,time_format)
+			duration = td(float(duration))
+			period = td(float(period))
+		except ValueError:
+			raise ValueError("The roster log could not be parsed.")
+
+		# Create the roster for this line
+		roster = Roster(name, next_start_time, duration, period)
+
+		# Append the task to our task list
+		roster_list.append(roster)
+
+	# Close the file
+	roster_file.close()
+
+	# Return the list of tasks
+	return roster_list
+
+########################################################
+# Write to the data files
+########################################################
+
+def update_logs(task_list, roster_list):
 	# Back up the task log file
-	copyfile(task_log_name, backup_log_name)
+	copyfile(task_log_name, backup_task_log_name)
 
 	# Blank out the file
 	clear_file(task_log_name)
@@ -183,7 +326,7 @@ def log_tasks(task_list):
 		raise IOError("The task log file could not be opened.")
 
 	for task in task_list:
-		# Convert from strings to the correct ftd.dt.daysormats
+		# Convert from strings to the correct formats
 		name = task.name
 		hours_spent = str(task.hours_spent)
 		start_time = dt.strftime(task.start_time,time_format)
@@ -200,6 +343,35 @@ def log_tasks(task_list):
 
 	# Close the task log file
 	task_file.close()
+
+	# Back up the roster log file
+	copyfile(roster_log_name, backup_roster_log_name)
+
+	# Blank out the file
+	clear_file(roster_log_name)
+
+	# Open the file
+	try:
+		roster_file = open(roster_log_name, "a")
+	except IOError:
+		raise IOError("The roster log file could not be opened.")
+
+	for roster in roster_list:
+		# Convert from strings to the correct formats
+		name = roster.name
+		next_start_time = dt.strftime(roster.next_start_time,time_format)
+		duration = str(roster.duration.days+roster.duration.seconds*days_per_second)
+		period = str(roster.period.days+roster.period.seconds*days_per_second)
+
+		# Create the list of each quantity and join into a single comma seperated string
+		roster_str_list = [name, next_start_time, duration, period]
+		roster_str = ",".join(roster_str_list)
+
+		# Add the roster to the roster log file
+		roster_file.write(roster_str+",\n")
+
+	# Close the roster log file
+	roster_file.close()
 
 ########################################################
 # Determining the recommended tasks
@@ -229,7 +401,6 @@ def sort_by_priority(task_list):
 		# Get the weighting to account for the time until the deadline
 		# This will be normalised by total_deadline_weight later
 		task.deadline_weight = np.exp(-abs(task.time_remaining().days+task.time_remaining().seconds*days_per_second))
-		print(task.deadline_weight)
 		total_deadline_weight += task.deadline_weight
 		
 		# Add up the number of hours spent so far
@@ -260,20 +431,25 @@ def sort_by_priority(task_list):
 # Actual interface
 ########################################################
 
-# Read in the log file
+# Read in the log files
 try:
 	task_list = read_tasks()
+	roster_list = read_rosters()
 except:
-	print("Could not read in the log file! It may be corrupted or missing. Exiting...")
+	print("Could not read in one of the log files! Either may be corrupted or missing. Exiting...")
 	exit()
 
 while True:
+	# Check if any rostered tasks need scheduling
+	task_list = Roster.check_all_rosters(roster_list, task_list)
+
 	# Query the user as to what they'd like to do:
 	print("")
 	print("TASK MANAGER:")	
 	print("0:		Get the top 5 things to do right now")
 	print("1:		Create a new task")
 	print("2:		Complete a task")
+	print("3:		Create a new task roster")
 	print("Anything else:	Exit")
 	option = input("Please enter your choice: ")
 	print("")
@@ -312,19 +488,24 @@ while True:
 				if task_id == "N":
 					break
 
+				# Otherwise check the ID is valid and if so mark the task as finished
 				task_id = int(task_id)
 				assert(task_id >= 0 and task_id < len(unfinished_tasks))
 				unfinished_tasks[task_id].finished = True
 				break
 			except:
 				print("Sorry, that task ID is not valid. Please try again: ")
-
+	# Create a new task roster
+	elif option == "3":
+		new_roster = Roster.make_roster()
+		roster_list.append(new_roster)
+		print("The task roster has been successfully created!")
 	else:
 		break
                                                                      
 # Write the log file
 try:
-	log_tasks(task_list)
+	update_logs(task_list, roster_list)
 except:
-	print("Could not write to the log file! It may be missing or there may be permission issues. Exiting...")
+	print("Could not write to the log files! Either may be missing or there may be permission issues. Exiting...")
 	exit()
